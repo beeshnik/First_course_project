3e98d50726c46f8c304575a603a4008187dae717
Михаил <mishafedoseev753@gmail.com>
2023-04-22 17:17:58 +0500

    22.04 Execute in preload.js

--- preload.js
+++ preload.js
@@ -5,13 +5,41 @@
  *
  * https://www.electronjs.org/docs/latest/tutorial/sandbox
  */
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-        const element = document.getElementById(selector)
-        if (element) element.innerText = text
-    }
 
-    for (const type of ['chrome', 'node', 'electron']) {
-        replaceText(`${type}-version`, process.versions[type])
-    }
+ window.addEventListener('DOMContentLoaded', () => {
+     const replaceText = (selector, text) => {
+         const element = document.getElementById(selector)
+         if (element) element.innerText = text
+     }
+
+     for (const type of ['chrome', 'node', 'electron']) {
+         replaceText(`${type}-version`, process.versions[type])
+     }
+
+     document.getElementById('button').addEventListener('click', ()=> {
+         const process = require('child_process');
+
+         const cmd = "start start.bat";
+
+         const outputElement = document.getElementById("output")
+         outputElement.value = null;
+         let child = process.spawn(cmd);
+
+         child.on('error', function(err){
+             outputElement.value += 'error: ' + err + '\n';
+         })
+
+         child.stdout.on('data', function (data){
+             outputElement.value += data + '\n';
+         })
+
+         child.stderr.on('data', function (data){
+             outputElement.value += 'stderr' + data + '\n'
+         })
+
+         child.on('close', function (code){
+             outputElement.value += 'command complete.'
+         })
+     })
 })
+
